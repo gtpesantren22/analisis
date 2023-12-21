57b5ead0767e9785cdb915485f381a711eeb4c1e
gtpesantren22 <cakroamt26@gmail.com>
2023-12-21 13:10:57 +0700

    update analisis lanjutan:

--- application/config/database.php
+++ application/config/database.php
@@ -83,7 +83,7 @@ $db['default'] = array(
 
 	// 'username' => 'u9048253_dwk',
 	// 'password' => 'PesantrenDWKIT2021',
-	// 'database' => 'u9048253_sentral',
+	// 'database' => 'u9048253_analisis',
 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
